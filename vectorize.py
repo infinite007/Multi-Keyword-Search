a98fa4d647effd7f08073b96985af7e973094059
infinite007 <cs12b1031@iith.ac.in>
2015-12-23 12:16:40 +0530

    Update vectorize.py

---
+++ vectorize.py
@@ -0,0 +1,21 @@
+from tfidf import rank
+from textblob import TextBlob as tb
+from DBSearch import DBSearch 
+
+"""
+This class implements the vectorization of the document.
+
+creates a dictionary which has word and its tfidf score as a key value pair. 
+"""
+class vectorize:
+	
+	def __init__(self):	
+		self.r = rank()
+		#self.log = Logger()
+		print 'vectorizer initialized!'
+	
+	def construct(self):
+		doc = DBSearch('foo','col').getAll()
+		
+		for obj in doc:
+			#TODO create a bloblist of doc['message']
